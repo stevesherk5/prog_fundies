1b4b6579414729054223fd78be6d4e6b6b89177e
Steve Sherk <steven.sherk@kellogg.northwestern.edu>
2024-01-27 20:45:02 +0000

    did the practice exercise. created two solutions - one worked for the stale transaction list. another one works if any changes/additions to the transaction list are required

--- practice.rb
+++ practice.rb
@@ -14,7 +14,8 @@ blockchain = [
   { "from_user" => "evan", "to_user" => "anthony", "amount" => 400 },
   { "from_user" => "ben", "to_user" => "anthony", "amount" => 1500 },
   { "from_user" => "anthony", "to_user" => "ben", "amount" => 4500 },
-  { "from_user" => "anthony", "to_user" => "evan", "amount" => 1750 }
+  { "from_user" => "anthony", "to_user" => "evan", "amount" => 1750 },
+  { "from_user" => "ben", "to_user" => "SHERK", "amount" => 1000000}
 ]
 
 # Write code below that returns the number of KelloggCoin that each user has in their 
@@ -28,20 +29,126 @@ blockchain = [
 
 # 👇👇👇 Your code HERE 👇👇👇
 
-# Find how much KelloggCoin Ben has 
+coin_owners = []
 
-ben_coin=0
-index=0
-puts blockchain[0]"#{from_user}"
-
-# loop do 
-#   blockchain[index][0]
-
-#   index = index + 1
+# Go through each transaction to extract data 
+for transaction in blockchain 
   
-#   # breaks loop if I've gone through the whole blockchain array 
-#   if index = blockchain.size
-#     break
-#   end 
-# end 
+  # find out the payer, receiver, and amount 
+  payer = transaction["from_user"]
+  receiver = transaction["to_user"]
+  amount = transaction["amount"]
+  
+  # set index to zero. this will help me find out which balances to update later
+  index = 0
 
+  # set payer/receiver variables. these will be updated later 
+  paycheck = 0
+  pay_row = 0
+  existing_payer_balance = 0
+  new_payer_balance = 0
+
+  reccheck = 0
+  rec_row = 0
+  existing_receiver_balance = 0
+  new_receiver_balance = 0
+
+  # go through coin_owners array and see whether the payer or receiver have an existing balance
+  # if they have an existing balance, then we'll use the current transaction amount to make 
+  # changes later 
+  for row in coin_owners
+    if row["name"]==receiver
+      reccheck = 1
+      rec_row = index
+      existing_receiver_balance = row["balance"]
+      new_receiver_balance = existing_receiver_balance + amount    
+    end
+    if row["name"]==payer
+      paycheck = 1
+      pay_row = index
+      existing_payer_balance = row["balance"]
+      new_payer_balance = existing_payer_balance - amount 
+    end
+    index = index+1
+  end
+
+  # if the receiver has an existing balance, update their balance based on the transaction amount
+  # if the receiver doesn't have a balance, create a new hash in the coin_owners array to establish
+  # the receiver's new balance 
+  if reccheck == 1
+    coin_owners[rec_row].replace("name"=>receiver,"balance"=>new_receiver_balance)
+  else 
+    coin_owners.push("name"=>receiver,"balance"=>amount)
+  end
+
+  # same process as above, but for the payers 
+  if paycheck == 1
+      coin_owners[pay_row].replace("name"=>payer,"balance"=>new_payer_balance)
+  else 
+    coin_owners.push("name"=>payer,"balance"=>-amount)
+  end
+   
+end
+
+# outputs the balance of every person who has transacted with this coin (except nil, in the ICO)
+for row in coin_owners
+  if row["name"] != nil
+    puts "#{row["name"]}'s KelloggCoin balance is #{row["balance"]}"
+  end
+end
+
+
+# ---------------------------
+# ALTERNATE WAY TO DO THE CODE. 
+# THIS METHOD IS NOT DYNAMIC, BUT DOES THE JOB GIVEN THE ORIGINAL DATA
+# ---------------------------
+
+# ben_coin=0
+# brian_coin=0
+# evan_coin=0
+# anthony_coin=0
+
+# for ben_transaction in blockchain 
+#   if ben_transaction["to_user"] == "ben"
+#     ben_coin = ben_coin + ben_transaction["amount"]
+#   end
+
+#   if ben_transaction["from_user"] == "ben"
+#     ben_coin = ben_coin - ben_transaction["amount"]
+#   end 
+# end
+
+# for brian_transaction in blockchain 
+#   if brian_transaction["to_user"] == "brian"
+#     brian_coin = brian_coin + brian_transaction["amount"]
+#   end
+
+#   if brian_transaction["from_user"] == "brian"
+#     brian_coin = brian_coin - brian_transaction["amount"]
+#   end 
+# end
+
+# for evan_transaction in blockchain 
+#   if evan_transaction["to_user"] == "evan"
+#     evan_coin = evan_coin + evan_transaction["amount"]
+#   end
+
+#   if evan_transaction["from_user"] == "evan"
+#     evan_coin = evan_coin - evan_transaction["amount"]
+#   end 
+# end
+
+# for anthony_transaction in blockchain 
+#   if anthony_transaction["to_user"] == "anthony"
+#     anthony_coin = anthony_coin + anthony_transaction["amount"]
+#   end
+
+#   if anthony_transaction["from_user"] == "anthony"
+#     anthony_coin = anthony_coin - anthony_transaction["amount"]
+#   end 
+# end
+
+# puts "Ben's KelloggCoin balance is #{ben_coin}"
+# puts "Brian's KelloggCoin balance is #{brian_coin}"
+# puts "Evan's KelloggCoin balance is #{evan_coin}"
+# puts "Anthony's KelloggCoin balance is #{anthony_coin}"
